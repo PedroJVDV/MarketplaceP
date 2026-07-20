cd0b3dbbd3d7aada88c3e599068b61a262be9c23
Pedro <scrowzer.graal2@gmail.com>
2026-07-20 19:47:11 -0300

    Runner moved from utils

--- marketplace/src/main/java/com/pedrojvdv/marketplace/SpringBootApplicationRunner.java
+++ marketplace/src/main/java/com/pedrojvdv/marketplace/SpringBootApplicationRunner.java
@@ -1,4 +1,4 @@
-package com.pedrojvdv.marketplace.utils;
+package com.pedrojvdv.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
