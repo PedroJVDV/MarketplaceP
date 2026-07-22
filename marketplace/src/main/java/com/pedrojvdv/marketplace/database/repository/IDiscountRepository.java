e67a1e4c50c5b85fcab4d95a027734c71ff6f3a3
Pedro <scrowzer.graal2@gmail.com>
2026-07-22 19:11:02 -0300

    JPQL + DTO projection ++

--- marketplace/src/main/java/com/pedrojvdv/marketplace/database/repository/IDiscountRepository.java
+++ marketplace/src/main/java/com/pedrojvdv/marketplace/database/repository/IDiscountRepository.java
@@ -2,12 +2,17 @@ package com.pedrojvdv.marketplace.database.repository;
 
 
 import com.pedrojvdv.marketplace.database.model.DiscountEntity;
-import com.pedrojvdv.marketplace.database.model.ProductEntity;
+import com.pedrojvdv.marketplace.dto.DiscountDto;
 import com.pedrojvdv.marketplace.enums.DiscountActive;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface IDiscountRepository extends JpaRepository<DiscountEntity, Long> {
 
@@ -15,4 +20,35 @@ public interface IDiscountRepository extends JpaRepository<DiscountEntity, Long>
 
     List<DiscountEntity> findByDiscountActive(DiscountActive discountActive);
 
+    @NullMarked
+    Optional<DiscountEntity> findById(Long id);
+
+    @Query("""
+            SELECT NEW com.pedrojvdv.marketplace.dto.DiscountDto(d.discountValue, d.discountActive)
+            FROM DiscountEntity d
+            WHERE d.discountValue = :discount_value
+            """)
+    List<DiscountDto> getByDiscountValue(BigDecimal discountValue);
+
+    @Query("""
+            SELECT NEW com.pedrojvdv.marketplace.dto.DiscountDto(d.discountValue, d.discountActive)
+            FROM DiscountEntity d
+            """)
+    List<DiscountDto> getAllDiscounts();
+
+    @Query("""
+            SELECT NEW com.pedrojvdv.marketplace.dto.DiscountDto(d.discountValue, d.discountActive)
+            FROM DiscountEntity d
+            WHERE d.discountActive = DiscountActive.YES
+            AND d.discountValue = :discount_value
+            """)
+    List<DiscountDto> getByActiveDiscount(DiscountActive discountActive);
+
+    @Query("""
+            SELECT NEW com.pedrojvdv.marketplace.dto.DiscountDto(d.discountValue, d.discountActive)
+            FROM DiscountEntity d
+            WHERE d.discountActive = DiscountActive.NO
+            AND d.discountValue = :discount_value
+            """)
+    List<DiscountDto> getByInativeDiscount(DiscountActive discountActive);
 }
