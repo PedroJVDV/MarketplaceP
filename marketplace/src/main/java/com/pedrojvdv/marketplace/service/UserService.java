ecba21399df8d9dd5a464f358d478856ca743f7f
Pedro <scrowzer.graal2@gmail.com>
2026-08-03 20:45:43 -0300

    Service ++ functions

--- marketplace/src/main/java/com/pedrojvdv/marketplace/service/UserService.java
+++ marketplace/src/main/java/com/pedrojvdv/marketplace/service/UserService.java
@@ -1,10 +1,105 @@
 package com.pedrojvdv.marketplace.service;
 
+import com.pedrojvdv.marketplace.database.model.UserEntity;
+import com.pedrojvdv.marketplace.database.repository.IUserRepository;
+import com.pedrojvdv.marketplace.dto.UserDto;
+import com.pedrojvdv.marketplace.enums.Role;
+import com.pedrojvdv.marketplace.exception.BadRequestException;
+import com.pedrojvdv.marketplace.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final IUserRepository userRepository;
+
+    public void createUser(UserDto userDto) throws BadRequestException {
+        UserEntity user = userRepository.findByEmail(userDto.getEmail())
+                .orElse(null);
+
+        if (user != null) {
+            throw new BadRequestException("Já existe um usuário cadastrado com esse email!");
+        }
+
+        userRepository.save(UserEntity.builder()
+                .name(userDto.getName())
+                .email(userDto.getEmail())
+                .password(userDto.getPassword())
+                .age(userDto.getAge())
+                .role(userDto.getRole())
+                .build());
+    }
+
+    public void updateUser(UserDto userDto, String email) throws NotFoundException {
+        userRepository.findByEmail(email)
+                .ifPresentOrElse(user -> {
+                            user.setName(userDto.getName());
+                            user.setEmail(userDto.getEmail());
+                            user.setPassword(userDto.getPassword());
+                            user.setAge(userDto.getAge());
+                            user.setRole(userDto.getRole());
+                            userRepository.save(user);
+                        },
+                        () -> {
+                            throw new NotFoundException("Usuário com esse email não existe!");
+                        });
+    }
+
+    public void deleteUser(String email, String password, UserDto userDto) throws NotFoundException {
+        userRepository.findByEmail(email)
+                .ifPresentOrElse(user -> {
+                    if (userDto.getPassword().equals(password) && user.getEmail().equals(email)) {
+                        userRepository.delete(user);
+                    } else {
+                        throw new NotFoundException("Senha incorreta!");
+                    }
+                }, () -> {
+                    throw new NotFoundException("Usuario com esse email não existe ou email incorreto!");
+                });
+    }
+
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public List<UserEntity> getUserByName(String username) throws NotFoundException {
+        List<UserEntity> user = userRepository.findByName(username);
+
+        if (user.isEmpty()) {
+            throw new NotFoundException("Não existe um usuário com este nome!");
+        }
+        return user;
+    }
+
+    public Optional<UserEntity> getUserByEmail(String email) throws NotFoundException {
+        Optional<UserEntity> user = userRepository.findByEmail(email);
+
+        if (user.isEmpty()) {
+            throw new NotFoundException("Não existe um usuário com este email!");
+        }
+        return user;
+    }
+
+    public Optional<UserEntity> getUserById(Long id) throws NotFoundException {
+        Optional<UserEntity> user = userRepository.findById(id);
+
+        if (user.isEmpty()) {
+            throw new NotFoundException("Nenhum usuário encontrado com este ID!");
+        }
+        return user;
+    }
+
+    //TODO: STATIC ROLES... (just thinking)
+    public Optional<UserEntity> getUserByRole(Role role) throws NotFoundException {
+        Optional<UserEntity> user = userRepository.findByRole(role);
+        if (user.isEmpty()) {
+            throw new NotFoundException("Usuário com a função especificada não existe!");
+        }
+        return user;
+    }
 }
