26f12b26bab86f02c56b7962e3e83dc343956093
Pedro <scrowzer.graal2@gmail.com>
2026-08-04 19:35:05 -0300

    id ++

--- marketplace/src/main/java/com/pedrojvdv/marketplace/dto/ProductDto.java
+++ marketplace/src/main/java/com/pedrojvdv/marketplace/dto/ProductDto.java
@@ -14,6 +14,8 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class ProductDto {
 
+    private Long id;
+
     private String name;
     private BigDecimal price;
     private Integer quantity;
