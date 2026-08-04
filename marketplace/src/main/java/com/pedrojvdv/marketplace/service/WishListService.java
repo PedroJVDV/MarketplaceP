c61623ede4976bbb382e4147d1f99b053eaeccb6
Pedro <scrowzer.graal2@gmail.com>
2026-08-04 19:35:40 -0300

    WishListService Functions ++

--- marketplace/src/main/java/com/pedrojvdv/marketplace/service/WishListService.java
+++ marketplace/src/main/java/com/pedrojvdv/marketplace/service/WishListService.java
@@ -11,6 +11,8 @@ import com.pedrojvdv.marketplace.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class WishListService {
@@ -32,6 +34,52 @@ public class WishListService {
                 .build());
     }
 
-    //TODO: wishList update ++++ functions... 04/08
+    public void updateWishList(WishListDto wishListDto) throws NotFoundException {
+        wishListRepository.findById(wishListDto.getId())
+                .ifPresentOrElse(wish -> {
+                    wish.setProduct(wish.getProduct());
+                    wish.setUser(wish.getUser());
+                    wishListRepository.save(wish);
+                }, () -> {
+                    throw new NotFoundException("Lista de desejos não encontrada!");
+                });
+    }
+
+    public void deleteWishList(WishListDto wishListDto, Long id) throws NotFoundException {
+        wishListRepository.findById(wishListDto.getId()).ifPresent(wish -> {
+            if (wishListDto.getId().equals(id)) {
+                wishListRepository.delete(wish);
+            } else {
+                throw new NotFoundException("ID da lista de desejos é inexistente!");
+            }
+        });
+    }
+
+    public List<WishListEntity> getAllWishLists() {
+        List<WishListEntity> wish = wishListRepository.findAll();
+
+        if (wish.isEmpty()) {
+            throw new NotFoundException("Nenhuma lista de desejos encontrada!");
+        }
+        return wish;
+    }
+
+    public List<WishListEntity> getWishListByUserId(Long userId) throws NotFoundException {
+        List<WishListEntity> wish = wishListRepository.findByUser_Id(userId);
+
+        if (wish.isEmpty()) {
+            throw new NotFoundException("Nenhuma lista de desejos encontrada para este usuário!");
+        }
+        return wish;
+    }
+
+    public List<WishListEntity> getWishListByUserEmail(String email) throws NotFoundException {
+        List<WishListEntity> wish = wishListRepository.findByUser_Email(email);
+
+        if (wish.isEmpty()) {
+            throw new NotFoundException("Nenhuma lista de desejo encontrada para este email de usuário!");
+        }
+        return wish;
+    }
 }
 
