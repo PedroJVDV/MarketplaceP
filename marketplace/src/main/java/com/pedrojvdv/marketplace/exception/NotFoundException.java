50ad37daca83f2ceffe97322ccb306a0ac000377
Pedro <scrowzer.graal2@gmail.com>
2026-08-17 21:05:47 -0300

    Exceptions ++

---
+++ marketplace/src/main/java/com/pedrojvdv/marketplace/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.pedrojvdv.marketplace.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
