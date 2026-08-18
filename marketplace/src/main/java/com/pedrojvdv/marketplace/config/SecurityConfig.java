a85a95fc3d04d7482a76dc3a2414ad8a4fa9d319
Pedro <scrowzer.graal2@gmail.com>
2026-08-18 19:06:53 -0300

    SecuritySmallC --

--- marketplace/src/main/java/com/pedrojvdv/marketplace/config/SecurityConfig.java
+++ marketplace/src/main/java/com/pedrojvdv/marketplace/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.pedrojvdv.marketplace.config;
 
+import com.pedrojvdv.marketplace.security.SecurityFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -31,6 +37,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/v1/product", "/v1/discount").hasRole("SELLER")
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
